d61632a5c1ce4e2e2cfa94aa1b65c4b0f6b2ee87
nendra <mrayhann@gmail.com>
2023-03-13 15:32:53 +0700

    chore: add eslint, prettier, husky, commitizen, and cz-conventional-changelog

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
+
+module.exports = config;
